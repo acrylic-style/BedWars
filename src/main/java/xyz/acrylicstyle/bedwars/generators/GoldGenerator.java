a3f9f7b40aa6910c2a09374ba1c90c963aa8c2d4
acrylic-style <me@acrylicstyle.xyz>
2019-09-18 12:10:31 +0900

    increase gold drop rate

--- src/main/java/xyz/acrylicstyle/bedwars/generators/GoldGenerator.java
+++ src/main/java/xyz/acrylicstyle/bedwars/generators/GoldGenerator.java
@@ -26,6 +26,6 @@ public class GoldGenerator implements Generator {
 
     @Override
     public double getGenerateTime() {
-        return 5/(Utils.teamSize/4F);
+        return 3/(Utils.teamSize/4F);
     }
 }
