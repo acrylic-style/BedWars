b6f83b9d94be0a7c252e7a4e2b9bb2ead7f045d7
acrylic-style <me@acrylicstyle.xyz>
2019-09-18 11:59:13 +0900

    remove loudly log

--- src/main/java/xyz/acrylicstyle/bedwars/tasks/EventTask.java
+++ src/main/java/xyz/acrylicstyle/bedwars/tasks/EventTask.java
@@ -6,14 +6,12 @@ import xyz.acrylicstyle.bedwars.utils.CollectionList;
 import xyz.acrylicstyle.bedwars.utils.Constants;
 import xyz.acrylicstyle.bedwars.utils.GameEvent;
 import xyz.acrylicstyle.bedwars.utils.Utils;
-import xyz.acrylicstyle.tomeito_core.utils.Log;
 
 public class EventTask extends BukkitRunnable {
     private CollectionList<GameEvent> events = new CollectionList<>();
 
     @Override
     public void run() {
-        long time = System.currentTimeMillis();
         GameEvent nextEvent = events.first();
         if (GameTask.playedTime > nextEvent.getTime()) {
             nextEvent.run();
@@ -21,7 +19,6 @@ public class EventTask extends BukkitRunnable {
             nextEvent = events.first();
         }
         Utils.setScoreReplace(nextEvent.getName() + " in " + ChatColor.GREEN + Utils.secondsToTime(nextEvent.getTime()-GameTask.playedTime), 10);
-        Log.debug("EventTask took " + (System.currentTimeMillis()-time) + "ms");
     }
 
     void scheduleEvents() {
