a5af6a89f54e9d57acf0344789ebe98e7474cbff
acrylic-style <me@acrylicstyle.xyz>
2019-09-20 08:33:52 +0900

    minor bug fix

--- src/main/java/xyz/acrylicstyle/bedwars/BedWars.java
+++ src/main/java/xyz/acrylicstyle/bedwars/BedWars.java
@@ -220,17 +220,17 @@ public class BedWars extends JavaPlugin implements Listener {
                                 boots = Utils.enchantTool(Utils.getColoredLeatherArmor(Material.LEATHER_BOOTS, BedWars.team.get(player.getUniqueId())), Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
                                 leggings = Utils.enchantTool(Utils.getColoredLeatherArmor(Material.LEATHER_LEGGINGS, BedWars.team.get(player.getUniqueId())), Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
                             } else if (armor == PlayerArmor.CHAIN) {
-                                boots = Utils.enchantTool(Utils.getColoredLeatherArmor(Material.CHAINMAIL_BOOTS, BedWars.team.get(player.getUniqueId())), Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
-                                leggings = Utils.enchantTool(Utils.getColoredLeatherArmor(Material.CHAINMAIL_LEGGINGS, BedWars.team.get(player.getUniqueId())), Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
+                                boots = Utils.enchantTool(Material.CHAINMAIL_BOOTS, Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
+                                leggings = Utils.enchantTool(Material.CHAINMAIL_LEGGINGS, Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
                             } else if (armor == PlayerArmor.IRON) {
-                                boots = Utils.enchantTool(Utils.getColoredLeatherArmor(Material.IRON_BOOTS, BedWars.team.get(player.getUniqueId())), Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
-                                leggings = Utils.enchantTool(Utils.getColoredLeatherArmor(Material.IRON_LEGGINGS, BedWars.team.get(player.getUniqueId())), Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
+                                boots = Utils.enchantTool(Material.IRON_BOOTS, Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
+                                leggings = Utils.enchantTool(Material.IRON_LEGGINGS, Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
                             } else if (armor == PlayerArmor.DIAMOND) {
-                                boots = Utils.enchantTool(Utils.getColoredLeatherArmor(Material.DIAMOND_BOOTS, BedWars.team.get(player.getUniqueId())), Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
-                                leggings = Utils.enchantTool(Utils.getColoredLeatherArmor(Material.DIAMOND_LEGGINGS, BedWars.team.get(player.getUniqueId())), Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
+                                boots = Utils.enchantTool(Material.DIAMOND_BOOTS, Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
+                                leggings = Utils.enchantTool(Material.DIAMOND_LEGGINGS, Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
                             } else { // impossible
-                                boots = Utils.enchantTool(Utils.getColoredLeatherArmor(Material.LEATHER_BOOTS, BedWars.team.get(player.getUniqueId())), Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
-                                leggings = Utils.enchantTool(Utils.getColoredLeatherArmor(Material.LEATHER_LEGGINGS, BedWars.team.get(player.getUniqueId())), Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
+                                boots = Utils.enchantTool(Material.LEATHER_BOOTS, Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
+                                leggings = Utils.enchantTool(Material.LEATHER_LEGGINGS, Enchantment.PROTECTION_ENVIRONMENTAL, ReinforcedArmor.tier);
                             }
                             player.getInventory().setBoots(boots);
                             player.getInventory().setLeggings(leggings);
