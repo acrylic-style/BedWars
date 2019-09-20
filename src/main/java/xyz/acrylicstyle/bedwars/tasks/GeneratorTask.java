09601b95c240d9956420e6d7eda9b62c4cd00b65
acrylic-style <me@acrylicstyle.xyz>
2019-09-20 11:37:05 +0900

    bug fix

--- src/main/java/xyz/acrylicstyle/bedwars/tasks/GeneratorTask.java
+++ src/main/java/xyz/acrylicstyle/bedwars/tasks/GeneratorTask.java
@@ -10,16 +10,24 @@ import java.util.Arrays;
 public class GeneratorTask extends BukkitRunnable {
     @Override
     public void run() {
-        Constants.generators.forEach(generator -> Arrays.asList(Team.values()).forEach(team -> {
-            ResourceGeneratorTask task = new ResourceGeneratorTask(generator);
-            if (generator instanceof GoldGenerator) ((GoldGenerator) generator).setTeam(team);
-            if (generator instanceof IronGenerator) ((IronGenerator) generator).setTeam(team);
-            task.setTeam(team);
-            task.runTaskTimer(Utils.getInstance(), 0, (long) (generator.getGenerateTime() * 20));
-            if (Constants.resourceGeneratorTask.get(team) == null) Constants.resourceGeneratorTask.put(team, new Collection<>());
-            Collection<Generator, ResourceGeneratorTask> collection = Constants.resourceGeneratorTask.get(team);
-            collection.put(generator, task);
-            Constants.resourceGeneratorTask.put(team, collection);
-        }));
+        Constants.generators.forEach(generator -> {
+            if (generator instanceof GoldGenerator || generator instanceof IronGenerator) {
+                Arrays.asList(Team.values()).forEach(team -> {
+                    ResourceGeneratorTask task = new ResourceGeneratorTask(generator);
+                    if (generator instanceof GoldGenerator) ((GoldGenerator) generator).setTeam(team);
+                    if (generator instanceof IronGenerator) ((IronGenerator) generator).setTeam(team);
+                    task.setTeam(team);
+                    task.runTaskTimer(Utils.getInstance(), 0, (long) (generator.getGenerateTime() * 20));
+                    if (Constants.resourceGeneratorTask.get(team) == null)
+                        Constants.resourceGeneratorTask.put(team, new Collection<>());
+                    Collection<Generator, ResourceGeneratorTask> collection = Constants.resourceGeneratorTask.get(team);
+                    collection.put(generator, task);
+                    Constants.resourceGeneratorTask.put(team, collection);
+                });
+            } else {
+                ResourceGeneratorTask task = new ResourceGeneratorTask(generator);
+                task.runTaskTimer(Utils.getInstance(), 0, (long) (generator.getGenerateTime() * 20));
+            }
+        });
     }
 }
